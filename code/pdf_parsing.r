27d8672cb4df7ee16acfefcf656b9396a59c119e
Jae Yeon Kim <jaeyeonkim@berkeley.edu>
2021-06-18 16:00:26 -0700

    update

---
+++ code/pdf_parsing.r
@@ -0,0 +1,41 @@
+# For Ubuntu installation, type this `sudo apt-get install libpoppler-cpp-dev` in the terminal
+
+# Import pkgs
+pacman::p_load(
+  pdftools,
+  tidyverse,
+  purrr,
+  tm,
+  here
+)
+
+# File name list
+filename <- list.files("here(raw_data/nclr)")
+
+# Reformat filename
+date_list <- gsub(".*_", "", filename)
+
+# Extract texts from the PDF
+text_list <- list(filename) %>%
+  pmap(~ pdf_text(.))
+
+# List into a data frame
+df <- text_list %>%
+  map_df(enframe, .id = "ListElement")
+
+# for loop
+
+for (i in seq(1:length(date_list))) {
+  df$ListElement[df$ListElement == paste(i)] <- date_list[i]
+}
+
+# Rename and mutate
+df <- df %>%
+  rename(
+    "date" = "ListElement",
+    "page_number" = "name"
+  )
+
+# Save the df
+
+saveRDS(df, here("processed_data/nclr.Rdata"))
